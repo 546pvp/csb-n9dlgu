2aa8dda6e3f02256053066d6e11d43ff91a2bd8f
Code Sandbox <hello@codesandbox.io>
2022-12-07 05:48:06 +0000

    Deployment from Codesandbox.

---
+++ script.js
@@ -0,0 +1,12 @@
+var cell = document.getElementsByClassName("grid-item");
+
+cell[0].onclick = function () {
+  this.style.backgroundColor = "black";
+  this.status = "black";
+};
+cell[1].onclick = function () {
+  this.style.backgroundColor = "black";
+};
+cell[2].onclick = function () {
+  this.style.backgroundColor = "black";
+};
